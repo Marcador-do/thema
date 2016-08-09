081f3226e777aa1f921e38509ae5425d1b06c480
Ronnie A. Baez Sesto <ronnie.baez@gmail.com>
2016-08-09 11:02:26 -0400

    Default page template and sidebar activation

---
+++ page.php
@@ -0,0 +1,35 @@
+<?php  
+/**
+ * [page.php]
+ *
+ * Page template.
+ *
+ * @link https://codex.wordpress.org/Theme_Development
+ * @author  Ronnie Baez <ronnie.baez@gmail.com>
+ * @package marcadordo
+ */
+?>
+<?php get_header(); ?>
+<div id="marcador-page-template" class="search">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12 div col-sm-12 col-md-12 col-lg-9">
+        <header class="page-header-template">
+          <h2 class="page-title">
+            <?php the_title(); ?>
+          </h2>
+        </header> 
+
+        <?php if (have_posts()) : while (have_posts()) : the_post();?>
+          <div class="page-content-template">
+            <?php the_content(); ?>
+          </div>
+        <?php endwhile; endif; ?>
+      </div>
+      <div class="col-xs-12 div col-sm-12 col-md-12 col-lg-3">
+      <?php get_sidebar(); ?>
+      </div>
+    </div>
+  </div>
+</div> 
+<?php get_footer(); ?>
