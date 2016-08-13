26437b81ba3065ab752d4d97b51ec2f1dde52c14
Ronnie A. Baez Sesto <ronnie.baez@gmail.com>
2016-08-13 19:22:14 -0400

    Removes update_count_callback, not needed

--- functions.php
+++ functions.php
@@ -166,7 +166,6 @@ function marcador_mail_taxonomy_function() {
 		'show_admin_column'          => false,
 		'show_in_nav_menus'          => false,
 		'show_tagcloud'              => false,
-		'update_count_callback'      => 'marcador_mail_taxonomy_count_function',
 	);
 	register_taxonomy( 'marcador_mail_taxonomy', array( 'marcador_mail_post' ), $args );
 
