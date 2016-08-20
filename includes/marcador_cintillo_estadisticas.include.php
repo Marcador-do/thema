e12a1151e384d4dc97f936c1f6d5bcaaee8b565d
Richard Blondet <richard.blondet@joystickinteractive.com>
2016-08-20 13:55:14 -0400

    adding cintillo completo con su funcionalidad y todo. Falta: los íconos del cintillo que borré sin querer. Próxima semana creo

--- includes/marcador_cintillo_estadisticas.include.php
+++ includes/marcador_cintillo_estadisticas.include.php
@@ -1,86 +1,174 @@
-<?php // Este es un layout temporal en lo que se maqueta el definitivo
+<?php 
+// 
+// Este es un layout temporal en lo que se maqueta el definitivo
+//
+$final_style = 'color:#f4f4f4; background-color: #333; border-right:1px solid #666; min-height: 150px;'; 
+?>
+<div id="template-cintillo-layout-marcador" class="container-fluid">
+	<div class="row">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-9">
+			<!-- #cintillo -->
+			<div id="cintillo">
+				<div class="cintillo-row">
+					<div class="cintillo-wrapper">
+						<div class="cintillo-container">
+							<div id="cintillo-selects-container">
+								<div id="cintillo-selects">
+									<select id="league" class="form-control">
+										<option value="mlb" selected="selected">MLB</option>
+										<option value="lidom">LiDom</option>
+									</select>
+									<select id="league_day" class="form-control">
+										<?php for ($i=0; $i < 3; $i++): $now = date('Y.m.d');  ?>
+											<option value="<?php echo date('Y.m.d', strtotime("-$i day")); ?>"<?php if( 0 == $i ) echo 'selected'; ?>>
+												<?php echo date('j M', strtotime( "-$i day" )); ?>
+											</option>
+										<?php endfor; ?>
+									</select>
+									
+								</div>
+							</div> 
+							<div id="cintillo-results-wrapper">
+								<div id="cintillo-results"><?php /*
+									DISPLAY RESULTS HERE
+									*/ ?>
+								</div>
+							</div>
+							
+							<div class="cintillo-next-btn">
+								<a href="#next">
+									<i class="material-icons md-24">chevron_right</i>
+								</a>
+							</div>
+						</div>
 
-$final_style = 'color:#f4f4f4; background-color: #333; border-right:1px solid #666; min-height: 150px;';
- ?>
-<div id="cintillo" class="container-fluid">
-  <div class="row">
-    <div class="col-xs-12 col-sm-12 col-lg-9">
-      <div class="container-fluid">
+					</div>
+				</div>
+			</div>
+			<!-- /#cintillo -->
+		</div>
+	</div>
+</div>
+<?php
+$stat_template = <<<STAT_TEMPLATE
+<div class="cintillo-result">
+	<a href="#" placetext="Resumen">
+		<h4 class="cintillo field title">STATUS</h4>
+		<p class="cintillo field team">
+			<span><img src="http://placehold.it/25" class="cintillo field image home"></span>&nbsp;&nbsp;
+			<span class="cintillo field name home">TEAM_A</span>&nbsp;&nbsp;&nbsp;
+			<span class="cintillo field score home">SCORE_A</span>
+		</p>
+		<p class="cintillo field team">
+			<span><img src="http://placehold.it/25" class="cintillo field image away"></span>&nbsp;&nbsp;
+			<span class="cintillo field name away">TEAM_B</span>&nbsp;&nbsp;&nbsp;
+			<span class="cintillo field score away">SCORE_B</span>
+		</p>
+	</a>
+</div>\n
+STAT_TEMPLATE;
+?>
+<script type="text/javascript">
+jQuery("#cintillo").ready(function() {
+	var processData = function (data) {
+		var result_width = 0;
+		jQuery("#cintillo-results").empty();
+		var stat_template = '<?php echo preg_replace( "/\r|\n/", "", $stat_template ); ?>';
+		// var stat_template = '<?php ob_start(); include( get_template_directory() . "/includes/marcador_cintillo_estadisticas_single.include.php" ); $string = ob_get_clean(); echo $string; ?>';
+		var lol = jQuery( stat_template );
+		var cintillo = data.cintillo;
 
-      <div class="row" style="background-color: #333; ">
-        <div class="col-xs-12 col-sm-12 col-lg-2" style="<?php echo $final_style; ?>">
-          <div style="height: 150px;display: flex; align-items: center;">
-            <select id="league" class="form-control">
-              <option value="mlb" selected>MLB</option>
-              <option value="lidom">LiDom</option>
-            </select>
+		for (var i = 0, len=cintillo.length; i < len; i++) {
+			var current = cintillo[i],
+				html = lol.clone();
+				result_width = result_width + 135;
 
-            <select id="league_day" class="form-control">
-              <?php for ($i=0; $i < 3; $i++): $now = date('Y.m.d');  ?>
-              <option value="<?php echo date('Y.m.d', strtotime("-$i day")); ?>" <?php if(0 == $i) echo 'selected'; ?> ><?php echo date('j M', strtotime("-$i day")); ?></option>
-            <?php endfor; ?>
-            </select>
-          </div>
+			html.find('h4.cintillo.field.title').text(current.status);
+			//html.find('span.cintillo.field.image.home').text(current.home.abbr);
+			html.find('span.cintillo.field.name.home').text(current.home.abbr);
+			html.find('span.cintillo.field.score.home').text(current.home.runs);
+			//html.find('span.cintillo.field.image.away').text(current.home.abbr);
+			html.find('span.cintillo.field.name.away').text(current.away.abbr);
+			html.find('span.cintillo.field.score.away').text(current.away.runs);
 
-        </div>
 
-        <div id="cintillo-results" class="col-xs-12 col-sm-12 col-lg-9" style="overflow-y: hidden; padding: 0; height: 150px;"> <!-- Results -->
-          
-        </div>
-        <div class="col-xs-12 col-sm-12 col-lg-1" style="<?php echo $final_style; ?>"><a href="#next">NEXT</a></div>
-      </div>
+			jQuery("#cintillo-results").append(html).css('width', result_width);
+			
+			// TODO: Remove loading
+			
+		};
+		// console.log(data);
+	};
 
-    </div>
-    </div>
-  </div>
-  <script type="text/javascript">
-  jQuery("#cintillo").ready(function() {
-    var processData = function (data) {
-      jQuery("#cintillo-results").empty();
-      var stat_template = '<?php ob_start(); include( get_template_directory() . "/includes/marcador_cintillo_estadisticas_single.include.php" ); $string = ob_get_clean(); echo $string; ?>';
-      var lol = jQuery(stat_template);
-      var cintillo = data.cintillo;
+	var getData = function (league, date, callback) {
+		jQuery.ajax({
+			url: '/wp-admin/admin-ajax.php',
+			type: 'post',
+			dataType: 'json',
+			data: {action: 'cintillo', league:league, date:date},
+			success: callback,
+			error: function(err) {console.log(err);}
+		});
+	};
 
-      for (var i = 0, len=cintillo.length; i < len; i++) {
-        var current = cintillo[i],
-            html = lol.clone();
+	var league  = jQuery("#league").val(),
+	current = jQuery("#league_day").val();
 
-        html.find('h4.cintillo.field.title').text(current.status);
-        //html.find('span.cintillo.field.image.home').text(current.home.abbr);
-        html.find('span.cintillo.field.name.home').text(current.home.abbr);
-        html.find('span.cintillo.field.score.home').text(current.home.runs);
-        //html.find('span.cintillo.field.image.away').text(current.home.abbr);
-        html.find('span.cintillo.field.name.away').text(current.away.abbr);
-        html.find('span.cintillo.field.score.away').text(current.away.runs);
+	jQuery("#league_day").change(function() {
+		// TODO: Show loading
+		league  = jQuery("#league").val()
+		current = jQuery(this).val();
+		getData(league, current, processData);
+	});
 
-        jQuery("#cintillo-results").append(html);
-        // TODO: Remove loading
-      };
-      console.log(data);
-    };
+	/**
+	 * Get ajax data
+	 */
+	getData(league, current, processData);
 
-    var getData = function (league, date, callback) {
-      jQuery.ajax({
-        url: '/wp-admin/admin-ajax.php',
-        type: 'post',
-        dataType: 'json',
-        data: {action: 'cintillo', league:league, date:date},
-        success: callback,
-        error: function(err) {console.log(err);}
-      });
-    };
+	/**
+	 * Keep a control for every scroll
+	 * @type {Number}
+	 */
+	var c = 0;
 
-    var league  = jQuery("#league").val(),
-        current = jQuery("#league_day").val();
+	/**
+	 * Handle the cintillo scrolling horizontally
+	 * @param  {obj} e)                {		var       _this         the obj scope
+	 * @param  {int} options.duration: 300           the duration in miliseconds
+	 * @param  {function} options.complete: function()    {		                                             	if( jQuery('#cintillo-results-wrapper').scrollLeft() + parent_result handle the callback result
+	 * @param  {string} {		                                                            duration: 300,		                                                                                       complete: function( on complete check if we need to go back
+	 * @return {function}                   callback to reselt c
+	 */
+	jQuery('.cintillo-next-btn [href="#next"]').click(function(e) {
+		var _this = this;
+		var parent_result = jQuery('#cintillo-results-wrapper').width();//important
+		var childs_result = jQuery('#cintillo-results').width();
 
-    jQuery("#league_day").change(function() {
-      // TODO: Show loading
-      league  = jQuery("#league").val()
-      current = jQuery(this).val();
-      getData(league, current, processData);
-    });
-
-    getData(league, current, processData);
-  });
-  </script>
+		e.preventDefault();
+		
+		if( c == 0 ) {
+			jQuery('#cintillo-results-wrapper').animate({
+		        scrollLeft: '+=135'
+		    },{
+		        duration: 300,
+		        complete: function() {
+		        	if( jQuery('#cintillo-results-wrapper').scrollLeft() + parent_result == childs_result ) {
+		        		c = 1;
+ 		        	}
+		        }
+		    });
+		} else {
+			jQuery('#cintillo-results-wrapper').animate({
+		        scrollLeft: '-=' + childs_result
+		    },{
+		        duration: 300,
+		        complete: function() {
+		        	c = 0;
+		        }
+		    });
+		}
+	});
+});
+</script>
 </div>
