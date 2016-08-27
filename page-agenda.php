1bc492704f723aec7bd1a77b2349afd5e09cbe20
Ronnie A. Baez Sesto <ronnie.baez@gmail.com>
2016-08-27 08:43:27 -0400

    Adds agenda page template with google calendar

--- page-agenda.php
+++ page-agenda.php
@@ -30,5 +30,25 @@
 ?>
 <?php /* <!-- /#marcador-navbar-submenu --> */ ?>
 
+<div id="marcador-page-template" class="search">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12 div col-sm-12 col-md-12 col-lg-9">
+        <header class="page-header-template">
+          <h2 class="page-title">
+            <?php wp_title($sep = '') ?>
+          </h2>
+        </header>
+        <br />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 div col-sm-12 col-md-12 col-lg-9 centered">
+
+        <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;mode=WEEK&amp;height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=6q97fnjocsid82pnq49ko6ehmk%40group.calendar.google.com&amp;color=%231B887A&amp;ctz=America%2FSanto_Domingo" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+      </div>
+    </div>
+  </div>
+</div>
 
 <?php get_footer(); ?>
