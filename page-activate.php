3bf45c098181b22bd46278e67d4e1ca2b7f1d286
Ronnie A. Baez Sesto <ronnie.baez@gmail.com>
2016-09-01 01:21:13 -0400

    Adds activation confirmation page

---
+++ page-activate.php
@@ -0,0 +1,42 @@
+<?php  
+/**
+ * [page-form.php]
+ *
+ * Page Form template.
+ *
+ * @link https://codex.wordpress.org/Theme_Development
+ * @author  Ronnie Baez <ronnie.baez@gmail.com>
+ * @package marcadordo
+ */
+
+  $key = "";
+  // If key not set, ignore and send to home
+  if ( ! isset( $_GET['k'] ) ) wp_redirect( home_url( '/' ) );
+
+  //do_action('marcador_activate_user'); // resides on marcador_manager plugin
+?>
+<?php get_header(); ?>
+<div id="marcador-page-template" class="search">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12 div col-sm-12 col-md-12 col-lg-9">
+        <header class="page-header-template">
+          <h2 class="page-title">
+            <?php the_title(); ?>
+          </h2>
+        </header> 
+      </div>
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <div class="col-xs-12 div col-sm-12 col-md-12 col-lg-9">
+        <div class="page-content-template">
+          <h3>Enhorabuena, <?php echo $user->display_name; ?>!</h3>
+          <p>Ya puedes colaborar en Marcador.do. <strong>Tu cuenta esta activada.</strong></p>
+          <p><a href="<?php echo home_url( '/' ); ?>"><?php echo home_url( '/' ); ?></a></p>
+          <pre><?php var_dump($is_verified); ?></pre>
+        </div>
+      </div>
+      <?php endwhile; endif; ?>
+    </div>
+  </div>
+</div> 
+<?php get_footer(); ?>
