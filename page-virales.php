c64ca30426e251e5492c16bde98b8b8a5ea3065a
Ronnie A. Baez Sesto <ronnie.baez@gmail.com>
2016-09-08 01:57:37 -0400

    Adds content to page virales pseudo random sizes

--- page-virales.php
+++ page-virales.php
@@ -30,5 +30,58 @@
 ?>
 <?php /* <!-- /#marcador-navbar-submenu --> */ ?>
 
+<?php 
+/**
+ * Virales
+ */
+$cat_dest = get_category_by_slug( 'virales' );
+$display_type = 1;  // TODO: Get from options
+$args = array(
+//  'category_name'    => 'destacado,destacadas',
+  'category__in' => array( $cat_dest->cat_ID, $cat_dests->cat_ID ), // TODO: Get from options
+  'post_type' => 'any',
+  
+  'post_status' => array(
+    'publish',
+  ),
+  'order'               => 'DESC',
+  'orderby'             => 'date',
+  'ignore_sticky_posts' => false,
+  'posts_per_page'         => 5,
+  'perm' => 'readable',
+);
+
+$virales = new WP_Query( $args ); ?>
+
+  <div class="container-fluid">
+    <div class="row">
+          <div class="col-xs-12 col-sm-12 col-lg-9">
+            <!-- Marcador posts -->
+            <div class="marcador-posts-listing-wrapper cards">
+              <div class="container-fluid">
+                <div class="row">
+
+        <?php $sizes = array(4,6,8,12);
+        while ( $virales->have_posts() ): $virales->the_post(); ?>
+          <?php if(isset($last)){
+            $width = $last;
+            unset($last);
+          } else {$width = $sizes[rand ( 0, 3 )];} ?>
+          <?php include ( get_template_directory() . "/includes/marcador_hero_post_list_item_card.include.php" ); ?>
+          <?php 
+            if($width == 12) unset($last);
+            else $last = 12 - $width;
+          ?>
+        <?php endwhile; ?>
+
+                </div>
+              </div>
+            </div>
+            <!-- .marcador-posts-listing -->
+          </div>
+    </div>
+  </div>
+
+
 
 <?php get_footer(); ?>
