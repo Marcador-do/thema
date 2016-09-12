fe095c2b3edf8dac3305e8d1afc19e5812226af6
Ronnie A. Baez Sesto <ronnie.baez@gmail.com>
2016-09-11 21:30:06 -0400

    Validates there are results for the category search

--- category.php
+++ category.php
@@ -30,41 +30,50 @@
 ?>
 <?php /* <!-- /#marcador-navbar-submenu --> */ ?>
 
-<?php
-$disciplina_id = get_category_by_slug( $disciplina )->cat_ID;
+<?php 
+$test = array();
 if (!isset($liga)) {
-  $all_ligas = get_categories( $args = array(
-    'taxonomy' => 'category',
-    'child_of' =>$disciplina_id)
-  );
-  $liga_id = $all_ligas[0]->cat_ID;
-  $test = array();
-  for ($i=0,$e=count($all_ligas); $i<$e; $i++) {
-    array_push($test, $all_ligas[$i]->cat_ID);
+  $category = get_category_by_slug( $disciplina );
+  if ($category !== false) {
+    $disciplina_id = $category->cat_ID;
+    $all_ligas = get_categories( $args = array(
+      'taxonomy' => 'category',
+      'child_of' =>$disciplina_id)
+    );
+
+    if (count($all_ligas) > 0) {
+      $liga_id = $all_ligas[0]->cat_ID;
+
+      for ($i=0,$e=count($all_ligas); $i<$e; $i++) {
+        array_push($test, $all_ligas[$i]->cat_ID);
+      }
+    }
   }
 } else {
   $liga_id = get_category_by_slug( $liga )->cat_ID;
-  $test = array($liga_id );
+  if ($category !== false) array_push($test, $liga_id);
 }
 
-$paged = get_query_var( 'paged', 1 );
-$args = array(
-  'category__in' => $test,
-  'post_type' => 'any',
-  
-  'post_status' => array(
-    'publish',
-  ),
-  'order'               => 'DESC',
-  'orderby'             => 'date',
-  'ignore_sticky_posts' => false,
-  'posts_per_page'         => 11,
-  'perm' => 'readable',
-  'paged' => $paged,
-);
-$principal = new WP_Query( $args ); ?>
+if (count($test) > 0) {
+  $paged = get_query_var( 'paged', 1 );
+  $args = array(
+    'category__in' => $test,
+    'post_type' => 'any',
+    
+    'post_status' => array(
+      'publish',
+    ),
+    'order'               => 'DESC',
+    'orderby'             => 'date',
+    'ignore_sticky_posts' => false,
+    'posts_per_page'         => 21,
+    'perm' => 'readable',
+    'paged' => $paged,
+  );
+  $principal = new WP_Query( $args );
+} ?>
 
-<?php while ( $principal->have_posts() ): $principal->the_post(); ?>
+<?php if ($principal): while ( $principal->have_posts() ): $principal->the_post(); ?>
 
   <?php if ( $principal->current_post === 0 ): ?>
     <?php if ( get_post_type() === "marcador_partido" ): ?>
@@ -87,7 +96,6 @@ $principal = new WP_Query( $args ); ?>
 
                 <div class="nav-previous alignleft"><?php next_posts_link( 'Older posts' ); ?></div>
                 <div class="nav-next alignright"><?php previous_posts_link( 'Newer posts' ); ?></div>
-                <?php echo $principal->max_num_pages; ?>
               </div>
             </div>
             <!-- .marcador-posts-listing -->
@@ -98,5 +106,21 @@ $principal = new WP_Query( $args ); ?>
     <script type="text/javascript">
 
     </script>
-
+<?php else: ?>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-lg-9">
+        <!-- Marcador posts -->
+        <div class="marcador-posts-listing-wrapper cards">
+          <div class="container-fluid">
+            <div class="row">
+              <h1>Nothing found</h1>
+            </div>
+          </div>
+        </div>
+        <!-- .marcador-posts-listing -->
+      </div> 
+    </div>
+  </div>
+<?php endif; ?>
 <?php get_footer(); ?>
