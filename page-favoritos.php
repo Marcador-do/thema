a4e13a4850834b1f29c88f559530334be85fdf26
mena142 <arturomega@gmail.com>
2016-10-18 15:36:19 -0400

    Style and structure changes.

---
+++ page-favoritos.php
@@ -0,0 +1,81 @@
+<?php  
+/**
+ * [page.php]
+ *
+ * Page template.
+ *
+ * @link https://codex.wordpress.org/Theme_Development
+ * @author  Ronnie Baez <ronnie.baez@gmail.com>
+ * @package marcadordo
+ */
+?>
+<?php get_header(); ?>
+<div id="marcador-page-template" class="search">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12 div col-sm-12 col-md-12 col-lg-9">
+         <div id="favoritos" class="row">
+                <article class="col-sm-6">
+              <div class="post">
+              <footer>
+              <h4 class="col-xs-6 title">Suarez: Messi was born great, Ronaldo made himself.</h4>
+              <div class="col-xs-6 tags">
+                  <span class="badge real-madrid"></span>
+                  <span class="badge fcb"></span>
+                  </div>
+            </footer>
+              </div>
+          </article> 
+                <article class="col-sm-6">
+              <div class="post">
+              <footer>
+              <h4 class="col-xs-6 title">Suarez: Messi was born great, Ronaldo made himself.</h4>
+              <div class="col-xs-6 tags"> <span class="badge real-madrid"></span>
+                  <span class="badge fcb"></span></div>
+            </footer>
+              </div>
+          </article> 
+        <article class="col-sm-6">
+              <div class="post">
+              <footer>
+              <h4 class="col-xs-6 title">Suarez: Messi was born great, Ronaldo made himself.</h4>
+              <div class="col-xs-6 tags">
+                  <span class="badge real-madrid"></span>
+                  <span class="badge fcb"></span>
+                  </div>
+            </footer>
+              </div>
+          </article> 
+        <article class="col-sm-6">
+              <div class="post">
+              <footer>
+              <h4 class="col-xs-6 title">Suarez: Messi was born great, Ronaldo made himself.</h4>
+              <div class="col-xs-6 tags">
+                  <span class="badge real-madrid"></span>
+                  <span class="badge fcb"></span>
+                  </div>
+            </footer>
+              </div>
+          </article> 
+        <article class="col-sm-6">
+              <div class="post">
+              <footer>
+              <h4 class="col-xs-6 title">Suarez: Messi was born great, Ronaldo made himself.</h4>
+              <div class="col-xs-6 tags">
+                  <span class="badge real-madrid"></span>
+                  <span class="badge fcb"></span>
+                  </div>
+            </footer>
+              </div>
+          </article> 
+            </div>
+  
+      </div>
+        
+     <!-- SIDEBAR -->
+      <?php get_sidebar(); ?>
+    
+    </div>
+  </div>
+</div> 
+<?php get_footer(); ?>
