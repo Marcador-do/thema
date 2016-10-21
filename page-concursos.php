426d6be0b9ff4a72249ca10555f178776d48b4de
mena142 <arturomega@gmail.com>
2016-10-20 23:47:18 -0400

    Front-page columns fixed,  Added view for "Concursos", Included ball icon for implementation

---
+++ page-concursos.php
@@ -0,0 +1,93 @@
+<?php  
+/**
+ * [page.php]
+ *
+ * Page template.
+ *
+ * @link https://codex.wordpress.org/Theme_Development
+ * @author  Ronnie Baez <ronnie.baez@gmail.com>
+ * @package marcadordo
+ */
+?>
+<?php get_header(); ?>
+<div id="marcador-page-template">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12 div col-sm-12 col-md-12 col-lg-9">
+          <h1 class="page-title">Concursos</h1>
+       <div class="concurso-wrapper">
+        <div class="row">
+            <div class="col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-body marcador-post-list-image" style="background-image: url('http://dev.marcador.do/wp-content/uploads/2016/09/NBA-Sindicato.jpg')"></div>
+                <div class="panel-body">
+                    <div class="marcador-post-list-title">
+                <a href="#">Echate unas risas de compania</a>
+                    </div>
+                    <div class="marcador-post-list-excerpt">
+                Santiago.- Los Leones de Santo Domingo se impusieron anoche 101-97, sobre los Metros de Santiago en el séptimo y último partido decisión de la gran final de la Liga Nacional de Baloncesto (LNB).
+                    </div>
+                </div>
+                <div class="panel-footer">
+                    <div class="row">
+                        <div class="col-md-3 col-xs-4">COMPARTE</div>
+                        <div class="col-md-3 col-xs-4">MAS INFO</div>
+                        <div class="col-md-push-3 col-md-3 col-xs-4 cerrado">CERRADO</div>
+                    </div>
+                </div>
+        </div>
+    </div>
+        </div>
+                
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="panel panel-default">
+                <div class="panel-body marcador-post-list-image" style="background-image: url('http://dev.marcador.do/wp-content/uploads/2016/09/NBA-Sindicato.jpg')"></div>
+                
+                <div class="panel-body">
+                    <div class="marcador-post-list-title">
+                <a href="#">Echate unas risas de compania</a>
+              </div>
+              <div class="marcador-post-list-excerpt">
+                Santiago.- Los Leones de Santo Domingo se impusieron anoche 101-97, sobre los Metros de Santiago en el séptimo y último partido decisión de la gran final de la Liga Nacional de Baloncesto (LNB).
+            </div>
+                    </div>
+                <div class="panel-footer">
+                    <div class="row">
+                <div class="col-md-3 col-xs-4 ">COMPARTE</div>
+                <div class="col-md-3 col-xs-4">MAS INFO</div>
+                <div class="col-md-push-3 col-md-3 col-xs-4 cerrado">CERRADO</div>
+                    </div>
+                </div>
+        </div></div> 
+            <div class="col-lg-6">
+                <div class="panel panel-default">
+                <div class="panel-body marcador-post-list-image" style="background-image: url('http://dev.marcador.do/wp-content/uploads/2016/09/NBA-Sindicato.jpg')"></div>
+                
+                <div class="panel-body">
+                    <div class="marcador-post-list-title">
+                <a href="#">Echate unas risas de compania</a>
+              </div>
+              <div class="marcador-post-list-excerpt">
+                Santiago.- Los Leones de Santo Domingo se impusieron anoche 101-97, sobre los Metros de Santiago en el séptimo y último partido decisión de la gran final de la Liga Nacional de Baloncesto (LNB).
+            </div>
+                    </div>
+                <div class="panel-footer">
+                    <div class="row">
+                <div class="col-md-3 col-xs-4 ">COMPARTE</div>
+                <div class="col-md-3 col-xs-4">MAS INFO</div>
+                <div class="col-md-push-3 col-md-3 col-xs-4 cerrado">CERRADO</div>
+                    </div>
+                </div>
+        </div>
+            </div>
+        </div>
+    </div>
+      </div>
+     
+      <?php get_sidebar(); ?>
+     
+    </div>
+  </div>
+</div> 
+<?php get_footer(); ?>
