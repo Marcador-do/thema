fab58830688347adc7160db2371e76f6d63d7d64
Ronnie A. Baez Sesto <ronnie.baez@gmail.com>
2016-10-29 10:35:15 -0400

    Basic html for sidebar and enables respective content

--- sidebar-marcador-user.php
+++ sidebar-marcador-user.php
@@ -1,7 +1,7 @@
-<?php if ( is_active_sidebar( 'front-page' ) ) : ?>
+<?php if ( is_active_sidebar( 'marcador-user' ) ) : ?>
 <div class="col-xs-12 div col-sm-12 col-md-12 col-lg-3">
 	<div class="row">
-		<?php dynamic_sidebar( 'front-page' ); ?>
+		<?php dynamic_sidebar( 'marcador-user' ); ?>
 	</div>
 </div>
 <?php endif; ?>
--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,7 @@
+<?php if ( is_active_sidebar( 'sidebar-estandar' ) ) : ?>
 <div class="col-xs-12 div col-sm-12 col-md-12 col-lg-3">
-
+	<div class="row">
+		<?php dynamic_sidebar( 'sidebar-estandar' ); ?>
+	</div>
 </div>
+<?php endif; ?>
