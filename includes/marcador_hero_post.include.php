e558312421457f307edaf491b4f1be05ca9478c1
Raylin Aquino <raylinaquino@gmail.com>
2016-12-29 13:17:52 -0400

    Fix header hero category

--- includes/marcador_hero_post.include.php
+++ includes/marcador_hero_post.include.php
@@ -7,6 +7,23 @@
         <a href="<?php the_permalink(); ?>" class="marcador-hero-permalink">
           <div class="hero-heading">
             <h1 class="heading"><?php the_title(); ?></h1>
+                  		<div class="marcador-post-list-meta">
+                		<div class="marcador-post-list-author">
+                			<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>">
+                				<?php echo get_the_author_meta( 'user_nicename' ); ?>
+                			</a>
+                		</div>
+                		<div class="marcador-post-list-date">
+                  			<a href="<?php echo esc_url( get_day_link( $year = get_the_date('Y') , $month = get_the_date('m'), $day = get_the_date('d') ) ) ?>">
+                    			<?php the_date('M d, Y', '<div class="meta-divisor"></div>', ''); ?>
+                  			</a> 
+            			</div>
+            			<!-- Conditional if favorite -->
+		                <div class="marcador-post-list-fav">
+		                	<i class="material-icons">star</i>
+		                </div>
+            			<!-- end conditional -->
+              		</div>
             <p class="excerpt"><?php the_excerpt(); ?></p>
           </div>
         </a>
