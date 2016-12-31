d6b136b0b7243a548977bf90baab16fc7c42c6c9
Edwin Marte <egm0815@gmail.com>
2016-12-31 15:09:51 -0400

    Eliminando scroll de los sidebar

--- front-page.php
+++ front-page.php
@@ -40,6 +40,7 @@ if ( has_nav_menu( 'primary_top' ) ) {
 
 	<div id="main-content" class="container-fluid">
 		<div class="the-content">
+
 				<!-- GRID START -->
 		        <?php the_content(); ?>
 		        <!-- GRID END -->
