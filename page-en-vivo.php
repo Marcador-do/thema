8bcf4db670c63e7e6f50e80136dd37a26b376e62
Edwin Marte <egm0815@gmail.com>
2017-01-01 17:21:26 -0400

    Nuevo template de sección en vivo

--- page-en-vivo.php
+++ page-en-vivo.php
@@ -11,49 +11,38 @@
 ?>
 <?php get_header(); ?>
 <div id="marcador-page-template">
-  <div id="main-content" class="container-fluid">
-    <div class="row">
-      <div class="col-lg-12">
-         <!--MARCADOR EN VIVO -->
-          <h1 class="page-title"><?php the_title(); ?></h1>
-          
-           <div id="broadcasting" class="row">
-            <span class="icon"><i class="material-icons">&#xE31D;</i></span><span class="tag">Estamos en vivo</span><span class="time">4:15</span><span class="cta">ENTRAR</span></div>
-          
-          <div id="en-vivo" class="row">
-        
-           
-        <div class="stream col-lg-12">
-        <iframe id="live" src="https://www.youtube.com/embed/1UdirOGTjOg" frameborder="0" allowfullscreen></iframe>
-            
-        <div class="banner col-sm-12 hidden-sm-down">ANUNCIO</div>
-            
-        <div class="encuesta col-sm-12">
-            <header class="row">
-                <span class="col-xs-4">ENCUESTA</span>
-                <span class="participantes col-xs-4">41 Participantes</span>
-                <span class="cta col-xs-4"><i class="material-icons">fast_forward</i>Ver Respuestas</span>
-            </header>
-            <div class="pregunta">Quien sera el mejor equipo que clasificara al  Clasico 2017?</div>
-            <p><input type="radio" id="brasil" value="1" name="pais"><label class="material-icon" for="brasil">Brasil</label><span class="barra"><b><i></i></b></span><span class="valor">3</span>
-            </p>
-            <p><input type="radio" id="colombia" value="2" name="pais"><label for="colombia">Colombia</label><span class="barra"><b><i></i></b></span><span class="valor">25</span>
-            </p>
-            <p><input type="radio" id="mexico" value="3" name="pais"><label for="mexico">Mexico</label><span class="barra"><b><i></i></b></span><span class="valor">5</span>
-            </p>
-            
-            
-            </div>
-        
-    </div>
-        </div>
-          <!--FIN MARCADOR EN VIVO -->
+  <div id="main-content" class="vivo container-fluid">
+    <div id="en-vivo" class="row">
+      <div   class="col-lg-12">
+           <!--MARCADOR EN VIVO -->
+            <h1 class="page-title"><?php the_title(); ?></h1>
       </div>
-        
-     <!-- SIDEBAR -->
-      <?php get_sidebar(); ?>
-    
-    </div>
-  </div>
+        <div class="col-lg-8">
+            <!-- div de alerta -->
+
+          <!--   <div id="broadcasting" class="row">
+              <span class="icon"><i class="material-icons">&#xE31D;</i></span><span class="tag">Estamos en vivo</span><span class="time">4:15</span><span class="cta">ENTRAR</span></div>-->
+          
+                <div class="video-container">
+                  <iframe id="live" src="https://www.youtube.com/embed/1UdirOGTjOg" frameborder="0" allowfullscreen></iframe>
+                </div>
+
+                <iframe width="100%" height="550px" scrolling="no" style="border: none;" src="http://www.opinionstage.com/polls/2410620/poll" frameBorder="0" name="os_frame" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>    
+                      <!--<div class="banner col-sm-12 hidden-sm-down">ANUNCIO</div>-->    
+           </div>
+      
+
+          <div class="chat col-lg-4">
+              <div id="chatenvivo"></div>
+
+              <br />
+
+          </div>       
+                 
+     </div>
+              <!--FIN MARCADOR EN VIVO -->
+   </div>
+</div>
+      
 </div> 
 <?php get_footer(); ?>
