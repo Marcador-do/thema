c0add9e7eca749d6c6e23a0ed7c9d0efa9e756d2
Edwin Marte <egm0815@gmail.com>
2017-01-24 09:57:47 -0400

    Agregando Google Analytics

--- header.php
+++ header.php
@@ -22,6 +22,10 @@ $authors = <<<AUTHORS
 *| Backend Development:
 *|  Ronnie Baez
 *|  ronnie.baez@gmail.com
+*|
+*| Web project manager:
+*|	Edwin Marte
+*|	egm0815@gmail.com
 *|==============================
 -->
 AUTHORS;
@@ -47,6 +51,19 @@ function banner() {
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	
 	<?php wp_head(); ?>
+
+	<!-- Google Analytic -->
+	<script>
+ 	 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  	ga('create', 'UA-68190205-1', 'auto');
+  	ga('send', 'pageview');
+
+</script>
+
 </head>
 <body <?php body_class(); ?> data-url='<?php echo get_site_url(); ?>'>
 	<script>
